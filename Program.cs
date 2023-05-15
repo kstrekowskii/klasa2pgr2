aa7a8dbac256ee96d7e68f24d66978491e8298d7
wesolykostek <jakub.kostera29@wp.pl>
2023-05-15 12:12:40 +0200

    version1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsola2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("klasa2p");
+            Console.WriteLine("grupa2");
+            Console.ReadLine();
+        }
+    }
+}
